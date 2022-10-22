c88c5788e9dce398e5ec5bd6d5ba4d90bfb29c6d
Rob Sharp <rob@sharp.id.au>
2022-10-22 17:00:31 +1100

    Add formatting

--- flake.nix
+++ flake.nix
@@ -1,31 +1,31 @@
 {
-	description = "Home Manager configuration of Rob Sharp";
+  description = "Home Manager configuration of Rob Sharp";
 
-	inputs ={
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
 
-		home-manager = {
-			url = "github:nix-community/home-manager/release-22.05";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+    home-manager = {
+      url = "github:nix-community/home-manager/release-22.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-	};
+  };
 
-	outputs = inputs @ { nixpkgs, home-manager, ... }:
-		let
-		system = "x86_64-linux";
-	pkgs = nixpkgs.legacyPackages.${system};
-	in {
-		homeConfigurations.qnm = home-manager.lib.homeManagerConfiguration {
-			inherit pkgs;
+  outputs = inputs@{ nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations.qnm = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
 
-			system = "x86_64-linux"; 
-			configuration.imports = [ ./home.nix ];
-			homeDirectory = "/home/qnm"; 
-			username = "qnm"; 
+        system = "x86_64-linux";
+        configuration.imports = [ ./home.nix ];
+        homeDirectory = "/home/qnm";
+        username = "qnm";
 
-# Optionally use extraSpecialArgs
-# to pass through arguments to home.nix
-		};
-	};
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
 }
--- home.nix
+++ home.nix
@@ -1,13 +1,25 @@
 { pkgs, ... }:
 
 {
-	programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 
-	home.packages = with pkgs; [
-		cowsay
-			neovim
-			firefox
-			git
-			gh
-	];
+  home.packages = with pkgs; [
+    cowsay
+    neovim
+    firefox
+    git
+    gh
+    gnome.nautilus
+    gnome.gnome-shell-extensions
+    gnomeExtensions.appindicator
+  ];
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      font-antialiasing = "rgba";
+    };
+
+    "org/gnome/desktop/input-sources" = { xkb-options = [ "ctrl:nocaps" ]; };
+  };
 }
