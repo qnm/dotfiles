69917e88db9eef489fd9336923937e5230f87e78
Robin Duncan Sharp <rob@sharp.id.au>
2023-06-13 09:40:21 +1000

    Remove podman for now

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,11 @@
 source ~/.asdf/asdf.fish
 
-fish_add_path /home/qnm/.cargo/bin
-fish_add_path /home/qnm/.gem/ruby/2.6.0/bin
+fish_add_path ~/.cargo/bin
+fish_add_path ~/.gem/ruby/2.6.0/bin
 
-set DOCKER_HOST unix://$XDG_RUNTIME_DIR/podman/podman.sock
+# set DOCKER_HOST unix://$XDG_RUNTIME_DIR/podman/podman.sock
+
+ #>>> conda initialize >>>
+ # !! Contents within this block are managed by 'conda init' !!
+ eval ~/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+ # <<< conda initialize <<<
