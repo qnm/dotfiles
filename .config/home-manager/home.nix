574f1fe77f53db6532aa60086f9182accab13e8e
Robin Duncan Sharp <rob@sharp.id.au>
2023-08-16 17:13:40 +1000

    Add nix to dotfiles

---
+++ .config/home-manager/home.nix
@@ -0,0 +1,153 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "qnm";
+  home.homeDirectory = "/home/qnm";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+
+    neofetch
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/qnm/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    withNodeJs = true;
+    withPython3 = true;
+    extraConfig = ''
+      set modeline
+      set number " turn on line numbers
+
+      " 80-col highlighting
+      highlight OverLength ctermbg=red ctermfg=white guibg=#592929
+      match OverLength /\%81v.\+/
+      set colorcolumn=80
+
+      " Identation
+      set autoindent " Copy indent from current line when starting a new line
+      set smarttab
+      set tabstop=2 " Number of space og a <Tab> character
+      set softtabstop=2
+      set shiftwidth=2 " Number of spaces use by autoindent
+      set expandtab " Pressing <Tab> puts spaces, and < and > for indenting uses psaces
+    '';
+
+    plugins = with pkgs.vimPlugins; [
+      {
+        plugin = ctrlp;
+        config = ''
+          let g:ctrlp_map = '<c-p>'
+          let g:ctrlp_cmd = 'CtrlP'
+          let g:ctrlp_custom_ignore = '\v[\/](node_modules|target|dist)|(\.(swp|ico|git|svn))$'
+        '';
+      }
+      {
+        plugin = catppuccin-nvim;
+        config = ''
+          " theme
+          syntax enable
+          set background=dark
+          colorscheme catppuccin-mocha
+          set termguicolors
+        '';
+      }
+      {
+        plugin = coc-nvim;
+        config = ''
+          let g:coc_global_extensions = ['coc-solargraph']
+          
+          inoremap <silent><expr> <TAB>
+                \ pumvisible() ? "\<C-n>" :
+                \ <SID>check_back_space() ? "\<TAB>" :
+                \ coc#refresh()
+          inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+          
+          function! s:check_back_space() abort
+            let col = col('.') - 1
+            return !col || getline('.')[col - 1]  =~# '\s'
+          endfunction
+
+          " Use <c-space> to trigger completion.
+          if has('nvim')
+            inoremap <silent><expr> <c-space> coc#refresh()
+          else
+            inoremap <silent><expr> <c-@> coc#refresh()
+          endif
+          
+          " Follow definition
+          nmap <silent> gd <Plug>(coc-definition)
+
+        '';
+      }
+      lightline-vim
+      coc-solargraph
+      coc-tsserver
+      coc-json
+    ];
+
+    extraPackages = with pkgs; [ fzf ];
+    extraPython3Packages = ps: with ps; [ /* python-language-server */ ];
+  };
+}
